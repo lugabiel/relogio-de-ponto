9841d3cbeacee16330801abae8c6ddba6e3f0d09
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-09-16 16:45:59 -0300

    route for continuous deployment
     The route '/get_update' :  Triggers a updates on the webapp hosted in pythonanywhere every commit to main on github. For this to work you need to set a webhook configuration in github.

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask.wrappers import Response
+from markupsafe import escape
+import git
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World</p>"
+# SQL setup
+connString = "mysql+mysqlconnector://<user>:<password>@<localhost[:3306/klipfolio"
+app.config["SQLALCHEMY_DATABASE_URI"] = connString
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+@app.route('/git_update', methods = ['POST'])
+def git_update():
+    repo = git.Repo('./relogio-de-ponto')
+    origin = repo.remotes.origin
+    repo.create_head(
+        'main',
+        origin.refs.main
+        ).set_tracking_branch(
+            origin.refs.main
+            ).checkout()
+    origin.pull()
+    return '',200
+
